66fc465079465ceb7e8a738629e92ef5dea03c4a
josdem <joseluis.delacruz@gmail.com>
2023-07-19 15:03:42 -0400

    Formatting missing files

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ const email = 'contact@josdem.io';
 const subject = 'Hello and welcome!';
 const body = 'This is the body of the email';
 
-const App = () => {  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Sneakers World Shopping
-        </p>
-      </header>
-      <ProductList/>
-      <footer className="App-footer">
-        <a href={`mailto:${email}?subject=${subject}&body=${body}`}>Contact Us</a>
-      </footer>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <p>Sneakers World Shopping</p>
+            </header>
+            <ProductList />
+            <footer className='App-footer'>
+                <a href={`mailto:${email}?subject=${subject}&body=${body}`}>Contact Us</a>
+            </footer>
+        </div>
+    );
+};
 
 export default App;
