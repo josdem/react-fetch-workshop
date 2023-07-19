fdf10ee6bafa62252d8c94e4af03d671dffb74fb
josdem <joseluis.delacruz@gmail.com>
2023-07-19 09:53:43 -0400

    #1 Adding mailto element to the footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import ProductList from './components/ProductList';
 
+const email = 'contact@josdem.io';
+const subject = 'Hello and welcome!';
+const body = 'This is the body of the email';
+
 const App = () => {  
   return (
     <div className="App">
@@ -10,6 +14,9 @@ const App = () => {
         </p>
       </header>
       <ProductList/>
+      <footer className="App-footer">
+        <a href={`mailto:${email}?subject=${subject}&body=${body}`}>Contact Us</a>
+      </footer>
     </div>
   );
 }
