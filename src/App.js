07b0a0a0cc54992b4f4941a21ac37440361bccf2
josdem <joseluis.delacruz@gmail.com>
2023-07-19 07:51:14 -0400

    Setting right header

--- src/App.js
+++ src/App.js
@@ -6,16 +6,8 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sneakers World Shopping
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <ProductList/>
     </div>
