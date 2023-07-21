cafd7c7f5c44043bb4e3fd4b3ed568daf8669cff
josdem <joseluis.delacruz@gmail.com>
2023-07-21 08:32:40 -0400

    #1 Exporting email, subject and body to constants

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
+import constants from './constants';
 import ProductList from './components/ProductList';
-
-const email = 'contact@josdem.io';
-const subject = 'Hello and welcome!';
-const body = 'This is the body of the email';
 
 const App = () => {  
   return (
@@ -15,7 +12,7 @@ const App = () => {
       </header>
       <ProductList/>
       <footer className="App-footer">
-        <a href={`mailto:${email}?subject=${subject}&body=${body}`}>Contact Us</a>
+        <a href={`mailto:${constants.email}?subject=${constants.subject}&body=${constants.body}`}>Contact Us</a>
       </footer>
     </div>
   );
