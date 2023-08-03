48aa00ba47ee51e7f03aef451897c768a6fabf07
Vansh Gandhi <vansh.vg18@gmail.com>
2023-08-03 12:04:00 +0530

    Configured ESLint

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { getProducts, currency } from '../products';
+import { getProducts, currency } from "../products";
 
-export default function ProductList() {
-    const [products, setProducts] = useState([]);
+export default function ProductList () {
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        getProducts().then((products) => {
-            setProducts(products);
-        });
-    }, []);
+  useEffect(() => {
+    getProducts().then((products) => {
+      setProducts(products);
+    });
+  }, []);
 
-    return (
-        <div data-testid='product-list' className='grid grid-cols-4 gap-5'>
-            {products.map((product) => (
-                <div key={product.sku}>
-                    <div className='flex'>{product.name}</div>
-                    <div className='flex-end'>{currency.format(product.price)}</div>
-                </div>
-            ))}
+  return (
+    <div data-testid="product-list" className="grid grid-cols-4 gap-5">
+      {products.map((product) => (
+        <div key={product.sku}>
+          <div className="flex">{product.name}</div>
+          <div className="flex-end">{currency.format(product.price)}</div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
--- src/mocks/mockFetch.js
+++ src/mocks/mockFetch.js
@@ -1,12 +1,14 @@
-export const getProducts = () => [
-    {
-        sku: 100,
-        name: 'Nike Air Max 2023',
-        price: 210,
-    },
-    {
-        sku: 101,
-        name: 'Stan Smith',
-        price: 110,
-    },
+const getProducts = () => [
+  {
+    sku: 100,
+    name: "Nike Air Max 2023",
+    price: 210,
+  },
+  {
+    sku: 101,
+    name: "Stan Smith",
+    price: 110,
+  },
 ];
+
+export default getProducts;
