1e304a8b10e699b293923633e8cb4b1c922403a1
josdem <joseluis.delacruz@gmail.com>
2023-08-13 17:16:20 -0400

    #11 Adding fetch mock test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
-import "./App.css";
-import constants from "./constants";
-import ProductList from "./components/ProductList";
+import React from 'react';
+import './App.css';
+import constants from './constants';
+import ProductList from './components/ProductList';
 
 const App = () => (
-  <div className="App">
-    <header data-testid="header" className="App-header">
-      <p>Sneakers World Shopping</p>
-    </header>
-    <ProductList />
-    <footer data-testid="footer" className="App-footer">
-      <a
-        href={`mailto:${constants.email}?subject=${constants.subject}&body=${constants.body}`}
-      >
-        Contact Us
-      </a>
-    </footer>
-  </div>
+    <div className='App'>
+        <header data-testid='header' className='App-header'>
+            <p>Sneakers World Shopping</p>
+        </header>
+        <ProductList />
+        <footer data-testid='footer' className='App-footer'>
+            <a
+                href={`mailto:${constants.email}?subject=${constants.subject}&body=${constants.body}`}
+            >
+                Contact Us
+            </a>
+        </footer>
+    </div>
 );
 
 export default App;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import { getProducts, currency } from "../products";
+import { getProducts, currency } from '../products';
 
-export default function ProductList () {
-  const [productsList, setProductsList] = useState([]);
+export default function ProductList() {
+    const [productsList, setProductsList] = useState([]);
 
-  useEffect(() => {
-    getProducts().then((products) => {
-      setProductsList(products);
-    });
-  }, []);
+    useEffect(() => {
+        getProducts().then((products) => {
+            setProductsList(products);
+        });
+    }, []);
 
-  return (
-    <div data-testid="product-list" className="grid grid-cols-4 gap-5">
-      {productsList.map((product) => (
-        <div key={product.sku}>
-          <div className="flex">{product.name}</div>
-          <div className="flex-end">{currency.format(product.price)}</div>
+    return (
+        <div data-testid='product-list' className='grid grid-cols-4 gap-5'>
+            {productsList.map((product) => (
+                <div key={product.sku}>
+                    <div className='flex'>{product.name}</div>
+                    <div className='flex-end'>{currency.format(product.price)}</div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
