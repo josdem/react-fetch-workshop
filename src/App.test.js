da53685711df0e5b2dda9115c85d6fb93206146d
josdem <joseluis.delacruz@gmail.com>
2023-08-13 14:12:06 -0400

    #11 Validating all main app components

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders app components", () => {
   render(<App />);
   const title = screen.getByText("Sneakers World Shopping");
   expect(title).toBeInTheDocument();
+  const header = screen.getByTestId("header");
+  expect(header).toBeInTheDocument();
+  const productList = screen.getByTestId("product-list");
+  expect(productList).toBeInTheDocument();
+  const footer = screen.getByTestId("footer");
+  expect(footer).toBeInTheDocument();
 });
